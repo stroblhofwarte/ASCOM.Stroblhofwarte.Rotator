2a50f4f8281cbff7d72b3f89ffd240cc9b5bf060
Othmar Ehrhardt <65812096+stroblhofwarte@users.noreply.github.com>
2021-06-09 17:57:20 +0200

    Update Rotator.ino
    License header added

--- Firmware/Rotator/Rotator.ino
+++ Firmware/Rotator/Rotator.ino
@@ -1,3 +1,24 @@
+/**************************************************************
+
+Arduino Firmware
+
+This file is part of the Stroblhofwarte.Rotator project 
+(https://github.com/stroblhofwarte/ASCOM.Stroblhofwarte.Rotator).
+Copyright (c) 2021, Othmar Ehrhardt, https://astro.stroblhof-oberrohrbach.de
+
+This program is free software: you can redistribute it and/or modify  
+it under the terms of the GNU General Public License as published by  
+the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+**************************************************************/
 
 #define STEP 3
 #define DIR  6
